ba074cf0ef36f2c8e2226da9a048eef0bc36378e
Neelabh Paul <npaul_be19@thapar.edu>
2023-12-04 23:56:03 +0530

    .

--- linkedList/merge2SortedLists.py
+++ linkedList/merge2SortedLists.py
@@ -1,30 +1,6 @@
 from LinkedList import Node, displayList
 from detectCycle import detectCycle
 
-# def merge2SortedLists(h1, h2):
-#     if h1.val > h2.val: merge2SortedLists(h2, h1)
-#     # print(__name__)
-#     # displayList(h1)
-#     # displayList(h2)
-#     prev = Node(-1)
-#     prev.next = h1
-#     p1, p2 = h1, h2
-#     while p2 and p1:
-#         if p1.val <= p2.val:
-#             p1 = p1.next
-#             prev = prev.next
-#         else:
-#             prev.next = p2
-#             prev = p2
-#             p2 = p2.next
-#             prev.next = p1
-#     prev.next = p2
-#     print(__name__, detectCycle(h1), detectCycle(h2))
-#     displayList(h1)
-#     # print(h1.val)
-#     return h1
-
-
 def merge2SortedLists(h1, h2):
 	dummyNode = Node(0)
 	tail = dummyNode
