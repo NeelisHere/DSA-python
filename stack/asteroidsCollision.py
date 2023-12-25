da3c91223a067858a22ae7d53e86e31293e61177
Neelabh Paul <npaul_be19@thapar.edu>
2023-12-25 18:48:38 +0530

    .

--- stack/asteroidsCollision.py
+++ stack/asteroidsCollision.py
@@ -1,13 +1,28 @@
 from collections import *
 
 def asteroidCollision(asteroids):
+    # Solution: 1
+    # s = deque()
+    # for x in asteroids:
+    #     while s and (s[-1] > 0 and x < 0):
+    #         if abs(s[-1]) == abs(x): s.pop(); break
+    #         elif abs(s[-1]) > abs(x): break
+    #         elif abs(s[-1]) < abs(x): s.pop()
+    #     else:
+    #         s.append(x)
+    # ans = list(s)
+    # print(ans)
+    
+    # Solution: 2 (recommended)
     s = deque()
     for x in asteroids:
-        while s and (s[-1] > 0 and x < 0):
-            if abs(s[-1]) == abs(x): s.pop(); break
-            elif abs(s[-1]) > abs(x): break
-            elif abs(s[-1]) < abs(x): s.pop()
-        else:
+        if not s or x > 0:
             s.append(x)
-    ans = list(s)
-    print(ans)
+        else:
+            while s and s[-1] > 0 and s[-1] < abs(x):
+                s.pop()
+            if s and s[-1] == abs(x):
+                s.pop()
+            else:
+                if not s or s[-1] < 0:
+                    s.append(x)
