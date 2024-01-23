6322442296c3c459dcafc1bae31a636306a61d9f
Neelabh Paul <npaul_be19@thapar.edu>
2024-01-23 21:14:04 +0530

    +DP/1D: Frog Jump Recursive + memoization

--- DP/1D/frogJump.py
+++ DP/1D/frogJump.py
@@ -1,3 +1,5 @@
+from collections import *
+
 
 def frogJump(n, h):
     dp = [0] * n
@@ -11,3 +13,20 @@ def frogJump(n, h):
             dp[i] = abs(h[i - 1] - h[i])
     print(dp)
 
+
+def frogJumpRec(n, h):
+    dp = defaultdict(lambda: -1)
+    dp[0] = 0
+    dp[1] = abs(h[0] - h[1])
+
+    def solve(i):
+        if dp[i] == -1:
+            dp[i] = min(
+                abs(h[i - 1] - h[i]) + solve(i - 1),
+                abs(h[i - 2] - h[i]) + solve(i - 2)
+            )
+        return dp[i]
+    res = solve(n - 1)
+    print(res)
+    
+    
