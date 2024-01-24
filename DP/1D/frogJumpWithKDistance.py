89063fbf356cb735be4083e6c97941af4467d0c7
Neelabh Paul <npaul_be19@thapar.edu>
2024-01-24 12:00:02 +0530

    +DP/1D: Frog jump with k distance

---
+++ DP/1D/frogJumpWithKDistance.py
@@ -0,0 +1,17 @@
+"""
+Frog with k distance / Minimal Cost: https://www.codingninjas.com/studio/problems/minimal-cost_8180930
+"""
+from typing import *
+
+
+def minimizeCost(k: int, a: List[int]) -> int:
+    n = len(a)
+    dp = [0] * n
+    for i in range(1, n):
+        res = float('inf')
+        for j in range(1, k + 1):
+            if i - j < 0: break
+            res = min(res, dp[i - j] + abs(a[i] - a[i - j]))
+        dp[i] = res
+    return dp[n - 1]
+
