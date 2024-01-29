4eb249ed5deeee00782b6c78b189c98403471391
Neelabh Paul <npaul_be19@thapar.edu>
2024-01-29 17:19:41 +0530

    code modified

--- DP/LCS/lcs.py
+++ DP/LCS/lcs.py
@@ -1,14 +1,28 @@
 
-def lcs(s1, s2):
+def lcsMemo(s1, s2):
     m, n = len(s1), len(s2)
     dp = [[-1]*(n + 1) for _ in range(m + 1)]
     
-    def calc(i, j):
-        if i<0 or j<0: return 0
+    def solve(i, j):
+        if i < 0 or j < 0:
+            return 0
         if dp[i][j] == -1:
-            if s1[i] == s2[j]: dp[i][j] = 1 + calc(i-1, j-1)
-            else: dp[i][j] = max(calc(i-1, j), calc(i, j-1))
+            if s1[i] == s2[j]:
+                dp[i][j] = 1 + solve(i-1, j-1)
+            else:
+                dp[i][j] = max(solve(i-1, j), solve(i, j-1))
         return dp[i][j]
+    print(res := solve(m-1, n-1))
+
+
+def lcsTab(s1: str, s2: str):
+    m, n = len(s1), len(s2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = 1 + dp[i - 1][j - 1]
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+                
         
-    print(res := calc(m-1, n-1))
-    return res
