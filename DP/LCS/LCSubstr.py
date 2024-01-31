8124cc3aba4092fb910159379c1cb5d2892281a9
Neelabh Paul <npaul_be19@thapar.edu>
2024-01-31 08:22:44 +0530

    code modified

--- DP/LCS/LCSubstr.py
+++ DP/LCS/LCSubstr.py
@@ -1,13 +1,27 @@
-from typing import *
+def longestCommonSubstringMemo(s1, s2):
+    res = []
 
-def LCSubstr(s1: str, s2: str) -> int:
-    n1, n2 = len(s1), len(s2)
-    t = [[0]*(n2 + 1) for _ in range(n1 + 1)]
+    def solve(i, j, count):
+        if i < 0 or j < 0:
+            return 0
+        if s1[i] == s2[j]:
+            return 1 + solve(i - 1, j - 1, count + 1)
+        else:
+            res.append(count)
+            return max(count, solve(i - 1, j, 0), solve(i, j - 1, 0))
+
+    solve(len(s1) - 1, len(s2) - 1, 0)
+    ans = max(res)
+    print(ans)
+
+
+def longestCommonSubstringTab(s1: str, s2: str) -> int:
+    m, n = len(s1), len(s2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
     res = 0
-    for i in range(1, n1 + 1):
-        for j in range(1, n2 + 1):
-            if s1[i-1] == s2[j-1]:
-                t[i][j] = 1 + t[i-1][j-1]
-            res = max(res, t[i][j])
-    
-    print(res)
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = 1 + dp[i - 1][j - 1]
+            res = max(res, dp[i][j])
+    return res
