6ef913b3e81894a23062d0b0e1b413e73e075fd9
Neelabh Paul <npaul_be19@thapar.edu>
2024-01-31 08:58:56 +0530

    code modified

--- DP/LCS/lps.py
+++ DP/LCS/lps.py
@@ -1,9 +1,16 @@
-from DP.LCS.lcs import lcs
 '''
 longest palindromic subsequence
 '''
 
-def lps(s):
+
+def longestPalindromicSubseq(s: str) -> int:
     s1, s2 = s, s[::-1]
-    print(res := lcs(s1, s2))
-    return res
+    m, n = len(s1), len(s2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = 1 + dp[i - 1][j - 1]
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    return dp[m][n]
