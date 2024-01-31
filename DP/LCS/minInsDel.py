4a1bf2cd904093cd66325e016b5b125fba91c429
Neelabh Paul <npaul_be19@thapar.edu>
2024-01-31 10:17:41 +0530

    code modified

--- DP/LCS/minInsDel.py
+++ DP/LCS/minInsDel.py
@@ -1,10 +1,11 @@
-from DP.LCS.lcs import lcs
+from DP.LCS.lcs import lcsTab
 '''
 Minimum Insertions/Deletions to Convert String A to String B
 '''
 
+
 def minOperations(s1, s2):
-    k = lcs(s1, s2)
+    k = lcsTab(s1, s2)
     res = (len(s1) - k) + (len(s2) - k)
     print(f'Min insertions/deletions to convert string {s1} to string {s2}: {res}')
     return res
--- DP/LCS/minInsertions.py
+++ DP/LCS/minInsertions.py
@@ -1,8 +1,10 @@
-from DP.LCS.lps import lps
+from DP.LCS.lps import longestPalindromicSubseq
 '''
 Minimum insertions to make string palindrome
+https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/
 '''
 
+
 def minInsertions(s):
-    print(res := len(s) - lps(s))
+    print(res := len(s) - longestPalindromicSubseq(s))
     return res
