3a338f0917c8277664c7393c93651da79faf6951
Neelabh Paul <npaul_be19@thapar.edu>
2024-02-01 19:37:02 +0530

    code modified

--- DP/LCS/editDistance.py
+++ DP/LCS/editDistance.py
@@ -1,13 +1,11 @@
-from typing import *
-
-'''
+"""
 https://leetcode.com/problems/edit-distance/
-Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2. 
+Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
 You have the following three operations permitted on a word:
     > Insert a character
     > Delete a character
     > Replace a character
-'''
+"""
 
 
 def minDistanceMemo(s1: str, s2: str):
--- DP/LCS/wildCardMatching.py
+++ DP/LCS/wildCardMatching.py
@@ -1,15 +1,13 @@
-from typing import *
-
-'''
+"""
 https://leetcode.com/problems/wildcard-matching/
 Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*' where:
     > '?' Matches any single character.
     > '*' Matches any sequence of characters (including the empty sequence).
 The matching should cover the entire input string (not partial).
-'''
+"""
 
 
-def wildCardMatching(s: str, p: str):
+def wildCardMatchingMemo(s: str, p: str):
     dp = [[-1]*(len(p) + 1) for _ in range(len(s) + 1)]
 
     def solve(i, j):
@@ -33,3 +31,21 @@ def wildCardMatching(s: str, p: str):
         
     res = solve(len(s)-1, len(p)-1)
     print(res)
+
+
+def wildCardMatchingTab(s, p):
+    m, n = len(p), len(s)
+    dp = [[False] * (m + 1) for _ in range(n + 1)]
+
+    dp[0][0] = True
+    for j in range(1, m + 1):
+        if p[j - 1] != '*':
+            break
+        dp[0][j] = True
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if p[j - 1] == '?' or s[i - 1] == p[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            elif p[j - 1] == '*':
+                dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
