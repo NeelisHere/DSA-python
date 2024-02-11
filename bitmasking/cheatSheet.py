2ace251997df10e831953026489889a4c05afb7d
Neelabh Paul <npaul_be19@thapar.edu>
2024-02-11 10:49:38 +0530

    bitmasking-cheatsheet

---
+++ bitmasking/cheatSheet.py
@@ -0,0 +1,39 @@
+import math
+
+def toBin(x, maxb=64):
+    res = ''
+    for i in range(maxb):
+        res += '1' if (x & (1 << i)) else '0'
+    return ''.join(list(reversed(res)))
+
+n_bit = lambda n: math.ceil(math.log2(n))
+get_bit = lambda n, i: 1 if (n & (1 << i)) else 0
+isOdd = lambda n: bool(n & 1)
+multiply_2powk = lambda n, k: n << k
+divide_2powk = lambda n, k: n >> k
+isPower2 = lambda n: bool(n & (n - 1) == 0)
+
+def set_bit(n, i):
+    n |= (1 << i)
+
+def clear_bit(n, i):
+    n &= ~(1 << i)
+
+def clear_rightmost_bit(n):
+    n &= (n - 1)
+
+def toggle_bit(n, i):
+    n ^= (1 << i)
+
+def clear_lastk(n, k):
+    n &= (-1 << k)
+
+def clear_range(n, l, r):
+    n &= (-1 << (l + 1)) | ~(-1 << (r - 1))
+
+def count_set_bits(n):
+    count, x = 0, n
+    while x:
+        x &= (x - 1)
+        count += 1
+    return count
